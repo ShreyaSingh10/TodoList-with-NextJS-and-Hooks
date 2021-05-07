c5d67b06215ed095486a79dfac2709bd2ef18215
Shreya Singh <shreyasingh@Shreyas-MacBook-Pro.local>
2021-05-07 15:51:36 +0530

    Adding route for details page

---
+++ Components/Details.js
@@ -0,0 +1,7 @@
+export default function Details(){
+    return (
+        <div>
+            <h2>Welcome to the details page</h2>
+        </div>
+    )
+}  
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,59 @@
-import { useState } from 'react';
+import Container from "../Components/Container.js";
 
-export default () => {
-const [todos, setTodos] = useState(["todo 1", "todo 2"]);
-const [todo, setTodo] = useState('');
-const removeTodo = todo => {
-  setTodos(todos.filter(t => t !== todo));
-};
-
-const handleClick =(e) => {
-  e.preventDefault(); //prevent form refresh
-  setTodos(todos => [...todos, todo] ); //updating the state
-  setTodo(todo => todo=""); //updating the state
-}
-
+export default function Main() {
   return (
-    <div> 
-      <form onSubmit={handleClick}>
-        <input value={todo} onChange={(e) => setTodo(e.target.value)}/>
-        <button>Add</button>  
-        {/* <button onClick={() => setTodos([...todos, todo])}> Add </button>*/}
-      </form>
-      {todos.map(todo => (
-        <>
-        <h1>{todo}</h1>
-        <button onClick={() => removeTodo(todo)}>remove</button> 
-       
-        </>
-      ))}
+    <div>
+      <Container/>
     </div>
   )
-};
+}
+
+//or export default Container;
+
+// import Container from "../Components/Container.js";
+// import Details from "../Components/Details.js";
+
+// function Main() {
+//   return (
+//     <div>
+//       <Container/>
+//       <Details/>
+//     </div>
+//   )
+// }
+ 
+// export default Main;
+
+// import { useState } from 'react';
+
+// export default function Container(){
+//   const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+//   const [todo, setTodo] = useState('');
+//   const removeTodo = todo => {
+//     setTodos(todos.filter(t => t !== todo));
+//   };
+
+//   console.log("TODOS", todos);
+
+//   const handleClick =(e) => {
+//     e.preventDefault(); //prevent form refresh
+//     setTodos(todos => [...todos, todo] ); //updating the state
+//     setTodo(todo => todo=""); //updating the state
+//   }
+
+//   return (
+//     <div> 
+//       <form onSubmit={handleClick}>
+//         <input value={todo} onChange={(e) => setTodo(e.target.value)}/>
+//         <button>Add</button>  
+//         {/* <button onClick={() => setTodos([...todos, todo])}> Add </button>*/}
+//       </form>
+//       {todos.map(todo => (
+//         <div>
+//         <h1>{todo}</h1>
+//         <button onClick={() => removeTodo(todo)}>remove</button> 
+//         </div>
+//       ))}
+//     </div>
+//   )
+// };
